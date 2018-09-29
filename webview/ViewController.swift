288d6fe3ab4612a0d12e52586e8594aa7684a6c4
abraham asmile <babyyuguioh@gmail.com>
2018-09-29 11:17:38 -0400

    Just a simple webview

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var URL = NSURL(string: "https://www.youtube.com/watch?v=SUXWAEX2jlg")
+        webView.loadRequest(NSURLRequest(url: URL! as URL) as URLRequest)
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
